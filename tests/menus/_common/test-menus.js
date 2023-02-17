edab0eadbc6ffbd2317380a8238a4377bc20ecd5
Nick Milton <nickdjm@gmail.com>
2023-02-17 14:13:36 -0500

    test: update tests to account for change to default selector

--- tests/menus/_common/test-menus.js
+++ tests/menus/_common/test-menus.js
@@ -26,26 +26,26 @@ export const twoLevelMenu = `
         <li id="item-2-0-0" class="nav-item dropdown">
           <a id="link-2-0-0" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Second item</a>
           <ul id="menu-2" class="dropdown-menu">
-            <li id="item-2-1-0" class="nav-item"><a id="link-2-1-0" class="dropdown-item" href="#">First item</a></li>
-            <li id="item-2-2-0" class="nav-item"><a id="link-2-2-0" class="dropdown-item" href="#">Second item</a></li>
-            <li id="item-2-3-0" class="nav-item"><a id="link-2-3-0" class="dropdown-item" href="#">Third item</a></li>
+            <li id="item-2-1-0"><a id="link-2-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-2-2-0"><a id="link-2-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-2-3-0"><a id="link-2-3-0" class="dropdown-item" href="#">Third item</a></li>
           </ul>
         </li>
         <li id="item-3-0-0" class="nav-item dropdown">
           <a id="link-3-0-0" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Third item</a>
           <ul id="menu-3" class="dropdown-menu">
-            <li id="item-3-1-0" class="nav-item"><a id="link-3-1-0" class="dropdown-item" href="#">First item</a></li>
-            <li id="item-3-2-0" class="nav-item"><a id="link-3-2-0" class="dropdown-item" href="#">Second item</a></li>
-            <li id="item-3-3-0" class="nav-item"><a id="link-3-3-0" class="dropdown-item" href="#">Third item</a></li>
+            <li id="item-3-1-0"><a id="link-3-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-3-2-0"><a id="link-3-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-3-3-0"><a id="link-3-3-0" class="dropdown-item" href="#">Third item</a></li>
           </ul>
         </li>
         <li id="item-4-0-0" class="nav-item"><a id="link-4-0-0" class="nav-link" href="#">Fourth item</a></li>
         <li id="item-5-0-0" class="nav-item dropdown">
           <a id="link-5-0-0" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Fifth item</a>
           <ul id="menu-5" class="dropdown-menu">
-            <li id="item-5-1-0" class="nav-item"><a id="link-5-1-0" class="dropdown-item" href="#">First item</a></li>
-            <li id="item-5-2-0" class="nav-item"><a id="link-5-2-0" class="dropdown-item" href="#">Second item</a></li>
-            <li id="item-5-3-0" class="nav-item"><a id="link-5-3-0" class="dropdown-item" href="#">Third item</a></li>
+            <li id="item-5-1-0"><a id="link-5-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-5-2-0"><a id="link-5-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-5-3-0"><a id="link-5-3-0" class="dropdown-item" href="#">Third item</a></li>
           </ul>
         </li>
       </ul>
@@ -65,31 +65,31 @@ export const fullMenu = `
         <li id="item-2-0-0" class="nav-item dropdown">
           <a id="link-2-0-0" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Second item</a>
           <ul id="menu-2" class="dropdown-menu" aria-labelledby="link-2-0-0">
-            <li id="item-2-1-0" class="nav-item"><a id="link-2-1-0" class="dropdown-item" href="#">First item</a></li>
-            <li id="item-2-2-0" class="nav-item dropdown">
+            <li id="item-2-1-0"><a id="link-2-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-2-2-0" class="dropdown">
               <a id="link-2-2-0" class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" href="#">Second item</a>
               <ul id="menu-2-2" class="dropdown-menu" aria-labelledby="link-2-2-0">
-                <li id="item-2-2-1" class="nav-item">
+                <li id="item-2-2-1">
                   <a id="link-2-2-1" class="dropdown-item" href="#">First item</a>
                 </li>
-                <li id="item-2-2-2" class="nav-item">
+                <li id="item-2-2-2">
                   <a id="link-2-2-2" class="dropdown-item" href="#">Second item</a>
                 </li>
-                <li id="item-2-2-3" class="nav-item">
+                <li id="item-2-2-3">
                   <a id="link-2-2-3" class="dropdown-item" href="#">Third item</a>
                 </li>
               </ul>
             </li>
-            <li id="item-2-3-0" class="nav-item dropdown">
+            <li id="item-2-3-0" class="dropdown">
               <a id="link-2-3-0" class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" href="#">Third item</a>
               <ul id="menu-2-3" class="dropdown-menu" aria-labelledby="link-2-3-0">
-                <li id="item-2-3-1" class="nav-item">
+                <li id="item-2-3-1">
                   <a id="link-2-3-1" class="dropdown-item" href="#">First item</a>
                 </li>
-                <li id="item-2-3-2" class="nav-item">
+                <li id="item-2-3-2">
                   <a id="link-2-3-2" class="dropdown-item" href="#">Second item</a>
                 </li>
-                <li id="item-2-3-3" class="nav-item">
+                <li id="item-2-3-3">
                   <a id="link-2-3-3" class="dropdown-item" href="#">Third item</a>
                 </li>
               </ul>
@@ -99,18 +99,18 @@ export const fullMenu = `
         <li id="item-3-0-0" class="nav-item dropdown">
           <a id="link-3-0-0" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Third item</a>
           <ul id="menu-3" class="dropdown-menu" aria-labelledby="link-3-0-0">
-            <li id="item-3-1-0" class="nav-item"><a id="link-3-1-0" class="dropdown-item" href="#">First item</a></li>
-            <li id="item-3-2-0" class="nav-item"><a id="link-3-2-0" class="dropdown-item" href="#">Second item</a></li>
-            <li id="item-3-3-0" class="nav-item"><a id="link-3-3-0" class="dropdown-item" href="#">Third item</a></li>
+            <li id="item-3-1-0"><a id="link-3-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-3-2-0"><a id="link-3-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-3-3-0"><a id="link-3-3-0" class="dropdown-item" href="#">Third item</a></li>
           </ul>
         </li>
         <li id="item-4-0-0" class="nav-item"><a id="link-4-0-0" class="nav-link" href="#">Fourth item</a></li>
         <li id="item-5-0-0" class="nav-item dropdown">
           <a id="link-5-0-0" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Fifth item</a>
           <ul id="menu-5" class="dropdown-menu" aria-labelledby="link-5-0-0">
-            <li id="item-5-1-0" class="nav-item"><a id="link-5-1-0" class="dropdown-item" href="#">First item</a></li>
-            <li id="item-5-2-0" class="nav-item"><a id="link-5-2-0" class="dropdown-item" href="#">Second item</a></li>
-            <li id="item-5-3-0" class="nav-item"><a id="link-5-3-0" class="dropdown-item" href="#">Third item</a></li>
+            <li id="item-5-1-0"><a id="link-5-1-0" class="dropdown-item" href="#">First item</a></li>
+            <li id="item-5-2-0"><a id="link-5-2-0" class="dropdown-item" href="#">Second item</a></li>
+            <li id="item-5-3-0"><a id="link-5-3-0" class="dropdown-item" href="#">Third item</a></li>
           </ul>
         </li>
       </ul>
