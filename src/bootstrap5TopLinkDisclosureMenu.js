a4a267f6d382497b8ff32463f855bcefb145dd51
Nick Milton <nickdjm@gmail.com>
2023-02-17 15:40:31 -0500

    feat(menu): add top link disclosure menu

---
+++ src/bootstrap5TopLinkDisclosureMenu.js
@@ -0,0 +1,107 @@
+import TopLinkDisclosureMenu from "accessible-menu/src/topLinkDisclosureMenu.js";
+import Bootstrap5TopLinkDisclosureMenuItem from "./bootstrap5TopLinkDisclosureMenuItem.js";
+import Bootstrap5TopLinkDisclosureMenuToggle from "./bootstrap5TopLinkDisclosureMenuToggle.js";
+
+/**
+ * An accessible disclosure menu in the DOM.
+ *
+ * See {@link https://www.w3.org/TR/wai-aria-practices-1.2/examples/disclosure/disclosure-navigation.html|Example Disclosure for Navigation Menus}
+ *
+ * @extends TopLinkDisclosureMenu
+ */
+class Bootstrap5TopLinkDisclosureMenu extends TopLinkDisclosureMenu {
+  /**
+   * The class to use when generating submenus.
+   *
+   * @type {typeof Bootstrap5TopLinkDisclosureMenu}
+   *
+   * @protected
+   */
+  _MenuType = Bootstrap5TopLinkDisclosureMenu;
+
+  /**
+   * The class to use when generating menu items.
+   *
+   * @type {typeof Bootstrap5TopLinkDisclosureMenuItem}
+   *
+   * @protected
+   */
+  _MenuItemType = Bootstrap5TopLinkDisclosureMenuItem;
+
+  /**
+   * The class to use when generating submenu toggles.
+   *
+   * @type {typeof Bootstrap5TopLinkDisclosureMenuToggle}
+   *
+   * @protected
+   */
+  _MenuToggleType = Bootstrap5TopLinkDisclosureMenuToggle;
+
+  /**
+   * Constructs the menu.
+   *
+   * @param {object}                                 options                                               - The options for generating the menu.
+   * @param {HTMLElement}                            options.menuElement                                   - The menu element in the DOM.
+   * @param {string}                                 [options.menuItemSelector = .nav-item,li]             - The CSS selector string for menu items.
+   * @param {string}                                 [options.menuLinkSelector = .nav-link,.dropdown-item] - The CSS selector string for menu links.
+   * @param {string}                                 [options.submenuItemSelector = .dropdown]             - The CSS selector string for menu items containing submenus.
+   * @param {string}                                 [options.submenuToggleSelector = .dropdown-toggle]    - The CSS selector string for submenu toggle buttons/links.
+   * @param {string}                                 [options.submenuSelector = .dropdown-menu]            - The CSS selector string for submenus.
+   * @param {string}                                 [options.submenuSubtoggleSelector = .dropdown-toggle] - The CSS selector string for submenu toggle buttons/links below the top level.
+   * @param {(HTMLElement|null)}                     [options.controllerElement = null]                    - The element controlling the menu in the DOM.
+   * @param {(HTMLElement|null)}                     [options.containerElement = null]                     - The element containing the menu in the DOM.
+   * @param {(string|string[]|null)}                 [options.openClass = show]                            - The class to apply when a menu is "open".
+   * @param {(string|string[]|null)}                 [options.closeClass = collapse]                       - The class to apply when a menu is "closed".
+   * @param {boolean}                                [options.isTopLevel = false]                          - A flag to mark the root menu.
+   * @param {(Bootstrap5TopLinkDisclosureMenu|null)} [options.parentMenu = null]                           - The parent menu to this menu.
+   * @param {string}                                 [options.hoverType = off]                             - The type of hoverability a menu has.
+   * @param {number}                                 [options.hoverDelay = 250]                            - The delay for closing menus if the menu is hoverable (in miliseconds).
+   * @param {boolean}                                [options.optionalKeySupport = false]                  - A flag to add optional keyboard support (Arrow keys, Home, and End) to the menu.
+   * @param {boolean}                                [options.initialize = true]                           - A flag to initialize the menu immediately upon creation.
+   */
+  constructor({
+    menuElement,
+    menuItemSelector = ".nav-item,li",
+    menuLinkSelector = ".nav-link,.dropdown-item",
+    submenuItemSelector = ".dropdown",
+    submenuToggleSelector = ".dropdown-toggle",
+    submenuSelector = ".dropdown-menu",
+    submenuSubtoggleSelector = ".dropdown-toggle",
+    controllerElement = null,
+    containerElement = null,
+    openClass = "show",
+    closeClass = "collapse",
+    isTopLevel = true,
+    parentMenu = null,
+    hoverType = "off",
+    hoverDelay = 250,
+    optionalKeySupport = false,
+    initialize = true,
+  }) {
+    super({
+      menuElement,
+      menuItemSelector,
+      menuLinkSelector,
+      submenuItemSelector,
+      submenuToggleSelector,
+      submenuSelector,
+      submenuSubtoggleSelector,
+      controllerElement,
+      containerElement,
+      openClass,
+      closeClass,
+      isTopLevel,
+      parentMenu,
+      hoverType,
+      hoverDelay,
+      optionalKeySupport,
+      initialize: false,
+    });
+
+    if (initialize) {
+      this.initialize();
+    }
+  }
+}
+
+export default Bootstrap5TopLinkDisclosureMenu;
---
+++ src/bootstrap5TopLinkDisclosureMenuItem.js
@@ -0,0 +1,46 @@
+/* eslint-disable jsdoc/no-undefined-types */
+
+import TopLinkDisclosureMenuItem from "accessible-menu/src/topLinkDisclosureMenuItem.js";
+
+/**
+ * A basic navigation link contained inside of a Bootstrap5TopLinkDisclosureMenu.
+ */
+class Bootstrap5TopLinkDisclosureMenuItem extends TopLinkDisclosureMenuItem {
+  /**
+   * Constructs the menu item.
+   *
+   * @param {object}                                     options                         - The options for generating the menu item.
+   * @param {HTMLElement}                                options.menuItemElement         - The menu item in the DOM.
+   * @param {HTMLElement}                                options.menuLinkElement         - The menu item's link in the DOM.
+   * @param {Bootstrap5TopLinkDisclosureMenu}            options.parentMenu              - The parent menu.
+   * @param {boolean}                                    [options.isSubmenuItem = false] - A flag to mark if the menu item is controlling a submenu.
+   * @param {Bootstrap5TopLinkDisclosureMenu|null}       [options.childMenu = null]      - The child menu.
+   * @param {Bootstrap5TopLinkDisclosureMenuToggle|null} [options.toggle = null]         - The controller for the child menu.
+   * @param {boolean}                                    [options.initialize = true]     - A flag to initialize the menu item immediately upon creation.
+   */
+  constructor({
+    menuItemElement,
+    menuLinkElement,
+    parentMenu,
+    isSubmenuItem = false,
+    childMenu = null,
+    toggle = null,
+    initialize = true,
+  }) {
+    super({
+      menuItemElement,
+      menuLinkElement,
+      parentMenu,
+      isSubmenuItem,
+      childMenu,
+      toggle,
+      initialize: false,
+    });
+
+    if (initialize) {
+      this.initialize();
+    }
+  }
+}
+
+export default Bootstrap5TopLinkDisclosureMenuItem;
